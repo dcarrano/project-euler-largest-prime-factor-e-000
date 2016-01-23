b4c5a30f4afad2a5c7dfc64387d6a50e36c195df
dcarrano <dcarrano@gmail.com>
2016-01-23 21:29:00 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,23 @@
-# Enter your procedural solution here!
+def prime?(number)
+
+i = 2
+
+until (number % i) == 0 || i == Math.sqrt(number).ceil
+  i += 1
+end
+
+if i == Math.sqrt(number).ceil then true else false end
+
+end
+
+def largest_prime_factor(input)
+
+current_largest = 0
+
+  for i in 2..Math.sqrt(input).ceil
+    if input % i == 0 && prime?(i) then current_largest = i end
+  end
+
+current_largest
+
+end
